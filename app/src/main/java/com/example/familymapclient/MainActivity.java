00cf94e6a2e6c8b0f7defadf3818d482dfd95bc8
brightlightkim <taeyangk95@naver.com>
2022-03-09 20:19:25 -0700

    created the login layout

--- app/src/main/java/com/example/familymapclient/MainActivity.java
+++ app/src/main/java/com/example/familymapclient/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.familymapclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import Request.LoginRequest;
 import Result.LoginResult;
@@ -11,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ServerProxy server = new ServerProxy();
-        server.setServerHost("localhost");
-        server.setServerPort("8080");
-        LoginRequest request = new LoginRequest("taeyang", "1234");
-        LoginResult result = server.login(request);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
 
     }
 }
